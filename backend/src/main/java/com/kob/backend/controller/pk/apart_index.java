aad42e53df3130c4199cbc640e1ad55fb0b1ba18
oldmomsimith <oldmomsimith@gmail.com>
2024-01-29 22:14:36 +0800

    对密码加密完成

--- backend/src/main/java/com/kob/backend/controller/pk/apart_index.java
+++ backend/src/main/java/com/kob/backend/controller/pk/apart_index.java
@@ -1,4 +1,4 @@
-package com.kob.backend.Controller.pk;
+package com.kob.backend.controller.pk;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
--- backend/src/main/java/com/kob/backend/controller/user/UserController.java
+++ backend/src/main/java/com/kob/backend/controller/user/UserController.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.kob.backend.mapper.UserMapper;
 import com.kob.backend.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,7 +38,9 @@ public class UserController {
 //        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 //        String encodedPassword = passwordEncoder.encode(password);
 //        User user = new User(userId, username, encodedPassword);
-        User user = new User(userId, username, password) ;
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder() ;
+        String encodedPassword = passwordEncoder.encode(password) ;
+        User user = new User(userId, username, encodedPassword) ;
         userMapper.insert(user);
         return "Add User Successfully";
     }
